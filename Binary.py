b11b669f52c131ffe36b3c6f4db29ebbd66fbb46
Taehyun Cho <tcho5@binghamton.edu>
2018-05-21 16:05:59 -0400

    Add files via upload

---
+++ Binary.py
@@ -0,0 +1,16 @@
+num = int(input())
+#Every odd values, Increment 1
+result = 0
+maximum = 0
+
+while num > 0:
+    if num % 2 == 1:
+        result += 1
+        if result > maximum:
+            maximum = result
+
+    else:
+        result = 0
+    num //= 2
+print(maximum)
+
