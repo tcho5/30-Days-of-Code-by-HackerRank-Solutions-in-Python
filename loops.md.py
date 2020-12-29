b19f4ddc52f7b0f5ccd21fce0fc5a4d176b9058d
ajitkr09 <ajitkumarjha1344@gmail.com>
2020-12-29 14:41:37 +0530

    added some spaces

---
+++ loops.md.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+import sys
+n = int(input().strip())
+for i in range(1, 11): ##Loops from 1 to 10
+    answer = (n * i)
+    print(str(n) + " x " + str(i) + " = " + str(answer))
+          ## + " x " + i + " = " + (N*i))
+
+
